4ad4d26a0dccb69431c7ec4336933f8f6507adbb
Ollie Davies <olliedaviess@me.com>
2021-12-01 13:59:00 +0000

    day1 part2

--- day1/main.rb
+++ day1/main.rb
@@ -9,3 +9,15 @@ data.each_with_index do |x, index|
 end
 
 puts "part1: #{number_of_increases}"
+
+
+number_of_increases = 0
+previous_reading = 0
+data.each_with_index do |x, i|
+  next if i == 0 || i+2 > data.length-1
+  current_reading = data[i-1..i+1].inject(:+)
+  number_of_increases+=1 if current_reading > previous_reading
+  previous_reading = current_reading
+end
+
+puts "part2: #{number_of_increases}"
