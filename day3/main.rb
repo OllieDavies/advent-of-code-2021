def481dca1772de8e53e3e83947696ee3fcd4d24
Ollie Davies <olliedaviess@me.com>
2021-12-03 14:28:06 +0000

    day3 part2

--- day3/main.rb
+++ day3/main.rb
@@ -11,3 +11,23 @@ data.transpose.each do |x|
 end
 
 puts "part1: #{gamma_rate.join.to_i(2) * epsilon_rate.join.to_i(2)}"
+
+def number_to_delete(reading, zeros, ones)
+  return zeros <= ones ? "0" : "1" if reading == "o2" 
+  return zeros > ones ? "0" : "1" if reading == "co2" 
+end
+
+def get_reading(data, type)
+  i = 0
+  until data.length == 1 do
+    bits = data.transpose[i]
+    zeros,ones = [bits.count("0"), bits.count("1")]
+    data.delete_if{ |n| n[i] == number_to_delete(type, zeros, ones) }
+    i+=1
+  end
+  data[0].join.to_i(2)
+end
+
+co2 = get_reading(data.clone, "co2")
+o2 = get_reading(data.clone, "o2")
+puts "part2: #{co2*o2}"
