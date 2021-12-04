2fd2ae6e8a016a723d7b256bdc6b6da5b31eb8cb
Ollie Davies <olliedaviess@me.com>
2021-12-04 15:36:10 +0000

    include ? in method names

--- day4/main.rb
+++ day4/main.rb
@@ -6,33 +6,33 @@ numbers_to_call = data[0].split(',').map(&:to_i)
 data.delete_at(0)
 boards = data.map{ |board| board.split(/\n/).map{ |n| n.split.map(&:to_i) } }
 
-def has_been_called(number, called_numbers)
+def has_been_called?(number, called_numbers)
   called_numbers.include?(number)
 end
 
 def called_status_from_array(row, called_numbers)
-  row.map{ |n| has_been_called(n, called_numbers) }
+  row.map{ |n| has_been_called?(n, called_numbers) }
 end
 
-def check_rows(rows, called_numbers)
+def check_rows?(rows, called_numbers)
   rows.each do |row|
     return true if called_status_from_array(row, called_numbers).all?(true)
   end
   return false
 end
 
-def has_board_won(board, called_numbers)
-  check_rows(board.clone, called_numbers) || check_rows(board.clone.transpose, called_numbers)
+def has_board_won?(board, called_numbers)
+  check_rows?(board.clone, called_numbers) || check_rows?(board.clone.transpose, called_numbers)
 end
 
 def get_uncalled(board, called_numbers)
-  board.flatten.reject{ |n| has_been_called(n, called_numbers) }
+  board.flatten.reject{ |n| has_been_called?(n, called_numbers) }
 end
 
 # numbers_to_call.each_with_index do |number, index|
 #   called_numbers = numbers_to_call[0..index]
 #   boards.each do |board|
-#     if has_board_won(board, called_numbers)
+#     if has_board_won?(board, called_numbers)
 #       puts "part1: #{get_uncalled(board, called_numbers).sum * number}"
 #       return
 #     end
@@ -43,7 +43,7 @@ last_board = boards[0]
 called_numbers = []
 numbers_to_call.each_with_index do |number, index|
   called_numbers = numbers_to_call[0..index]
-  remaining_boards = boards.clone.reject{ |board| has_board_won(board, called_numbers) }
+  remaining_boards = boards.clone.reject{ |board| has_board_won?(board, called_numbers) }
   if remaining_boards.length > 0
     last_board = remaining_boards[0]
   else
