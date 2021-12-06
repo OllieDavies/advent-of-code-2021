8e977cf46d896684abe3ea8c4fac448b15402a17
Ollie Davies <olliedaviess@me.com>
2021-12-06 20:40:49 +0000

    day6 part1

---
+++ day6/main.rb
@@ -0,0 +1,25 @@
+require 'csv'
+
+fish = CSV.read('input.txt', converters: :numeric)[0]
+
+count_by_days = fish.group_by(&:itself).transform_values(&:count)
+count_by_days.default = 0
+
+day = 0
+until day == 80
+  ready = count_by_days[0]
+  count_by_days[0] = 0
+
+  for i in 0..8
+    count_by_days[i] = count_by_days[i+1]
+  end
+
+  count_by_days[6] += ready
+  count_by_days[8] += ready
+
+  day += 1
+end
+
+
+
+puts "part1: #{count_by_days.values.sum}"
