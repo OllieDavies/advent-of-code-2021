de299fa07d177334463a080cc4f5e5389ab8098f
Ollie Davies <olliedaviess@me.com>
2021-12-07 18:37:05 +0000

    day7 part2

--- day7/main.rb
+++ day7/main.rb
@@ -13,3 +13,17 @@ until current_position == positions.count do
 end
 
 puts "part1: #{[weights.min_by{ |k, v| v }].to_h}"
+
+def get_consumption(current_position, i)
+  distance = (i - current_position).abs
+  Array.new(distance).map.with_index{ |n, x| x+1 }.sum
+end
+
+current_position, weights = 0, Hash.new
+until current_position == positions.count do
+  weighted = positions.map.with_index{ |n, i| n * get_consumption(current_position, i) }
+  weights[current_position] = weighted.sum
+  current_position += 1
+end
+
+puts "part2: #{[weights.min_by{ |k, v| v }].to_h}"
