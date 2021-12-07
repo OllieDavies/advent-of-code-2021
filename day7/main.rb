6cbc3cc78ec8dbb494cae85f6e87d92f58e5ffac
Ollie Davies <olliedaviess@me.com>
2021-12-07 17:40:32 +0000

    refactor day7 part1

--- day7/main.rb
+++ day7/main.rb
@@ -5,19 +5,11 @@ data = CSV.read('input.txt', converters: :numeric)[0]
 grouped_crabs = data.group_by(&:itself).transform_values(&:count)
 positions = Array.new(grouped_crabs.keys.max+1).map.with_index{ |n, i| grouped_crabs[i] || 0 }
 
-i = 0
-weights = Hash.new
-until i == positions.count do
-  # get the crabs to the left and right of i
-  left = positions[0..i];
-  right = positions[i..positions.count]
-
-  # weight each crab, based on it's current distance from i
-  left_weighted = left.reverse.map.with_index{ |n, j| n * j }.sum
-  right_weighted = right.map.with_index{ |n, j| n * j }.sum
-
-  weights[i] = left_weighted + right_weighted
-  i += 1
+current_position, weights = 0, Hash.new
+until current_position == positions.count do
+  weighted = positions.map.with_index{ |n, j| n * (j - current_position).abs  }
+  weights[current_position] = weighted.sum
+  current_position += 1
 end
 
 puts "part1: #{[weights.min_by{ |k, v| v }].to_h}"
